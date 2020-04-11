c4b97da95f8409a3069af3c46176503204e517fe
99c3nts <1783.hydro@gmail.com>
2020-04-11 02:22:17 -0400

    general tweaks

--- App.js
+++ App.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 
-import CredsInput from './components/CredsInput';
-import LoginButton from './components/LoginButton';
-import SignupButton from './components/SignupButton';
-import ForgotButton from './components/ForgotButton';
-import ContactUsButton from './components/ContactUsButton';
+import Login from './pages/Login';
+// import CredsInput from './components/CredsInput';
+// import LoginButton from './components/LoginButton';
+// import SignupButton from './components/SignupButton';
+// import ForgotButton from './components/ForgotButton';
+// import ContactUsButton from './components/ContactUsButton';
 
-export default class App extends React.Component {
+const App = () =>{
+  return (
+          <ImageBackground source={require('./assets/background.png')}
+                            style={styles.container}>
+              <Text style={styles.logo}>Groceries To Go</Text>
+              <Login />
+              {/* <LoginButton />
+              <SignupButton />
+              <ForgotButton />
+              <ContactUsButton /> */}
+          </ImageBackground>
+        );
 
-  render(){
-    return (
-      <ImageBackground source={require('./assets/bg.png')}
-                        style={styles.container}>
-          <Text style={styles.logo}>GROCERIES TO GO</Text>
-          <LoginButton />
-          <SignupButton />
-          <ForgotButton />
-          <ContactUsButton />
+}
+
+// export default class App extends React.Component {
+
+//   render(){
+//     return (
+//       <ImageBackground source={require('./assets/background.png')}
+//                         style={styles.container}>
+//           <Text style={styles.logo}>Groceries To Go</Text>
+//           <CredsInput />
+//           {/* <LoginButton />
+//           <SignupButton />
+//           <ForgotButton />
+//           <ContactUsButton /> */}
         
 
-      </ImageBackground>
-    );
-  }
-}
+//       </ImageBackground>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -37,24 +54,27 @@ const styles = StyleSheet.create({
     fontFamily: 'sans-serif-light',
     fontSize:40,
     color:"#fff",
-    marginTop:200,
-    marginBottom:0
+    justifyContent: 'center',
+    marginTop:500,
+    marginBottom: 100
   },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#465881",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"white",
-    fontSize:11
-  },
+  // inputView:{
+  //   width:"80%",
+  //   backgroundColor:"#465881",
+  //   borderRadius:25,
+  //   height:50,
+  //   marginBottom:20,
+  //   justifyContent:"center",
+  //   padding:20
+  // },
+  // inputText:{
+  //   height:50,
+  //   color:"white"
+  // },
+  // forgot:{
+  //   color:"white",
+  //   fontSize:11
+  // },
 });
+
+export default App
